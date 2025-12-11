36a7420dcb9caa93ae4e80b8b0af3332044447b1
Lavish <lavishkumar@outlook.in>
2025-12-11 15:29:53 +0530

    Refactor Fenwick tree implementation

--- rangeQ/fentree.cpp
+++ rangeQ/fentree.cpp
@@ -4,34 +4,27 @@
 
 struct Fenwick {
     int n;
-    vector<long long> bit1, bit2;
+    vector<long long> bit;
 
-    Fenwick(int n): n(n), bit1(n + 1), bit2(n + 1) {}
+    Fenwick(int n) : n(n), bit(n + 1, 0) {}
 
-    void update(vector<long long>& bit, int i, long long val) {
+    // add 'val' to index i (0-indexed externally)
+    void update(int i, long long val) {
         for (++i; i <= n; i += i & -i)
             bit[i] += val;
     }
 
-    long long query(vector<long long>& bit, int i) {
+    // prefix sum [0..i]
+    long long prefixSum(int i) {
         long long sum = 0;
         for (++i; i > 0; i -= i & -i)
             sum += bit[i];
         return sum;
     }
 
-    void rangeAdd(int l, int r, long long val) {
-        update(bit1, l, val);
-        update(bit1, r + 1, -val);
-        update(bit2, l, val * (l));
-        update(bit2, r + 1, -val * (r + 1));
-    }
-
-    long long prefixSum(int i) {
-        return query(bit1, i) * (i + 1) - query(bit2, i);
-    }
-
+    // range sum [l..r]
     long long rangeSum(int l, int r) {
-        return prefixSum(r) - prefixSum(l - 1);
+        if (r < 0) return 0;
+        return prefixSum(r) - (l ? prefixSum(l - 1) : 0);
     }
 };
